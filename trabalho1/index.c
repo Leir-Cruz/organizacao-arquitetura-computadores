938916af07fe20c9279e5b950fd48c75da4ff5d5
Gabriel Cruz <gabrielcruzvazsantos@gmail.com>
2023-04-24 19:31:28 -0300

    making R-type output

--- trabalho1/index.c
+++ trabalho1/index.c
@@ -7,8 +7,9 @@ int main() {
     int32_t imm32;
 
     //Let´s put some of the mask we will use the most
-    uint32_t seven_bits_mask = 0x7f;
-    uint32_t five_bits_mask = 0x1f;
+    uint32_t seven_bits_mask = 0x7f; //0111 1111
+    uint32_t five_bits_mask = 0x1f; //0001 1111
+    uint32_t three_bits_mask = 0x7; //0111
 
     //read instruction and identify it using opcode
     scanf("%x", &RI);
@@ -19,16 +20,16 @@ int main() {
 
         funct7 = RI >> 25 & seven_bits_mask;
         rs2 = RI >> 20 & five_bits_mask;
-        //rs1 = rs1 >> 25 & five_bits_mask;
-        //funct7 = funct7 >> 25 & seven_bits_mask;
-        //funct7 = funct7 >> 25 & seven_bits_mask;
+        rs1 = RI >> 15 & three_bits_mask;
+        funct3 = RI >> 12 & three_bits_mask;
+        rd = RI >> 7 & seven_bits_mask;
 
         printf("opcode: %x\n", opcode);
         printf("funct7: %x\n",funct7 );
         printf("rs2: %x\n", rs2);
-        //printf("rs1: %x\n", rs1);
-        //printf("funct3: %x\n", funct3);
-        //printf("rd: %x\n", rd);
+        printf("rs1: %x\n", rs1);
+        printf("funct3: %x\n", funct3);
+        printf("rd: %x\n", rd);
 
     //checks if it´s an I-type instruction
     } else if (opcode == 0x13) {
