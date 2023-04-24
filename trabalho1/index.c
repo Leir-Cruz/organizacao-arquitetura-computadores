39b2f50e1fb6e9cda291ff57d63b29b3f7fb9648
Gabriel Cruz <gabrielcruzvazsantos@gmail.com>
2023-04-24 17:08:14 -0300

    being able to read only opcode

--- trabalho1/index.c
+++ trabalho1/index.c
@@ -6,6 +6,13 @@ int main() {
     uint32_t RI, funct7, funct3, opcode, rd, rs1, rs2, shamt;
     int32_t imm32;
 
+    //Lets put some of the mask we will use the most
+    uint32_t last_7_bits_mask = 0x7f;
+
+    //read instruction and identify it using opcode
+    scanf("%x", &RI);
+    printf("%x", (RI & last_7_bits_mask));
+
 
 
     return 0;
